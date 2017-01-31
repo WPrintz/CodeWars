5cbcac82146447a97422663b79d4a1ed06ed15c1
Wallace Printz <jnoholds@gmail.com>
2017-01-31 13:23:51 -0600

    Fixed error in test suite

--- 5kyu_Sum_of_Pairs.py
+++ 5kyu_Sum_of_Pairs.py
@@ -67,12 +67,12 @@ l8= [5, 9, 13, -3]
 l9= [0]*10**7 ; l9[-1] = 3 ; l9[-2] = 5
 
 
-# print(sum_pairs(l1, 8) == [1, 7]) #, "Basic: %s should return [1, 7] for sum = 8" % l1)
-# print(sum_pairs(l2, -6) == [0, -6]) #, "Negatives: %s should return [0, -6] for sum = -6" % l2)
-# print(sum_pairs(l3, -7) == None) #, "No Match: %s should return None for sum = -7" % l3)
-# print(sum_pairs(l4, 2) == [1, 1]) #, "First Match From Left: %s should return [1, 1] for sum = 2 " % l4)
-# print(sum_pairs(l5, 10) == [3, 7]) #, "First Match From Left REDUX!: %s should return [3, 7] for sum = 10 " % l5)
-# print(sum_pairs(l6, 8) == [4, 4]) #, "Duplicates: %s should return [4, 4] for sum = 8" % l6)
-# print(sum_pairs(l7, 0) == [0, 0]) #, "Zeroes: %s should return [0, 0] for sum = 0" % l7)
-# print(sum_pairs(l8, 10) == [13, -3]) #, "Subtraction: %s should return [13, -3] for sum = 10" % l8)
-print(sum_pairs(l9, 8) == [1, 1]) #, "Long array: %s should return [13, -3] for sum = 10" % l8)
+print(sum_pairs(l1, 8) == [1, 7]) #, "Basic: %s should return [1, 7] for sum = 8" % l1)
+print(sum_pairs(l2, -6) == [0, -6]) #, "Negatives: %s should return [0, -6] for sum = -6" % l2)
+print(sum_pairs(l3, -7) == None) #, "No Match: %s should return None for sum = -7" % l3)
+print(sum_pairs(l4, 2) == [1, 1]) #, "First Match From Left: %s should return [1, 1] for sum = 2 " % l4)
+print(sum_pairs(l5, 10) == [3, 7]) #, "First Match From Left REDUX!: %s should return [3, 7] for sum = 10 " % l5)
+print(sum_pairs(l6, 8) == [4, 4]) #, "Duplicates: %s should return [4, 4] for sum = 8" % l6)
+print(sum_pairs(l7, 0) == [0, 0]) #, "Zeroes: %s should return [0, 0] for sum = 0" % l7)
+print(sum_pairs(l8, 10) == [13, -3]) #, "Subtraction: %s should return [13, -3] for sum = 10" % l8)
+print(sum_pairs(l9, 8) == [5, 3]) #, "Long array: %s should return [13, -3] for sum = 10" % l8)
