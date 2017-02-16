8c3fc8e30ffef2c9496b7f8a8445def8db06d226
Wallace Printz <jnoholds@gmail.com>
2017-02-15 21:49:56 -0600

    Remove file

--- for_matt.py
+++
@@ -1,8 +0,0 @@
-def bouncy_ratio(percent):
-    is_bouncy = lambda n: any(a < b for a, b in zip(n, n[1:])) \
-                        and any(a > b for a, b in zip(n, n[1:]))
-    n, bouncy = 99, 0
-    while bouncy < percent * n:
-        n += 1
-        bouncy += is_bouncy(str(n))
-    return n
